de12dfbd1dfe5f4ee7d119f1ed869cd95fa04e7c
mizdra <pp.mizdra@gmail.com>
2022-03-07 00:32:46 +0900

    update magic-trackpad-switcher

--- Formula/magic-trackpad-switcher.rb
+++ Formula/magic-trackpad-switcher.rb
@@ -1,8 +1,8 @@
 class MagicTrackpadSwitcher < Formula
   desc "The tool to switch Magic Trackpad connections between multiple Mac devices"
   homepage "https://github.com/mizdra/magic-trackpad-switcher"
-  url "https://github.com/mizdra/magic-trackpad-switcher.git", tag: "v0.0.2", revision: "092e7024b6eaf9895c51d3b2d55321684d576179"
-  head "https://github.com/mizdra/magic-trackpad-switcher.git", branch: "master"
+  url "https://github.com/mizdra/magic-trackpad-switcher.git", tag: "v0.0.3", revision: "1592875d18acccf4274c77fa9ad263d1e7dbd137"
+  head "https://github.com/mizdra/magic-trackpad-switcher.git", branch: "main"
 
   depends_on "blueutil"
 
@@ -10,12 +10,8 @@ class MagicTrackpadSwitcher < Formula
     bin.install "magic-trackpad-switcher"
   end
 
-  plist_options manual: "magic-trackpad-switcher"
-
   service do
     run [opt_bin/"magic-trackpad-switcher"]
-    run_type :interval
-    interval 2
     keep_alive true
   end
 
