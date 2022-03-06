44a1a3d01e9b7f2fd0225a048df5fec11b083b50
mizdra <pp.mizdra@gmail.com>
2022-03-07 01:06:24 +0900

    fix path

--- Formula/magic-trackpad-switcher.rb
+++ Formula/magic-trackpad-switcher.rb
@@ -13,8 +13,8 @@ class MagicTrackpadSwitcher < Formula
   service do
     run [opt_bin/"magic-trackpad-switcher"]
     keep_alive true
-    log_path "#{HOMEBREW_PREFIX}/var/log/beanstalkd.log"
-    error_log_path "#{HOMEBREW_PREFIX}/var/log/beanstalkd.error.log"
+    log_path "#{HOMEBREW_PREFIX}/var/log/magic-trackpad-switcher.log"
+    error_log_path "#{HOMEBREW_PREFIX}/var/log/magic-trackpad-switcher.error.log"
   end
 
   test do
